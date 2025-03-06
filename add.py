3ae7e90a9e0d48ac7b9a8fa366c9da625b0512a4
layyacinematics <contactlayyac@gmail.com>
2025-03-06 13:37:33 +0530

    added python file and readme file for that

---
+++ add.py
@@ -0,0 +1,8 @@
+# Add two numbers
+result = 5 + 10
+
+# Print the result
+print("The sum is:", result)
+
+# Print thank you message
+print("Thank you")
---
+++ readme.md
@@ -0,0 +1,1 @@
+This python script can add 5 and 10 together and say thank you
